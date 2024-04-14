56bfc8b90f0b52b571a60b90d8ce8f0459d9db35
Vinay <vinaykgarg720@gmail.com>
2024-04-14 11:17:05 +0530

    registor warn

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import { mobile } from '../responsive';
-import {useDispatch,useSelector} from 'react-redux';
+import {useDispatch} from 'react-redux';
 import { register } from '../redux/apiCalls';
 const Container = styled.div`
   width: 100vw;
